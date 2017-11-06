fe9c2cc821077fd5870aff905a166a85838d3587
Vanessa Perry <vanessap1590@gmail.com>
2017-11-06 16:29:44 -0500

    Update MagicClientDriver.java

--- src/client/MagicClientDriver.java
+++ src/client/MagicClientDriver.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.FileNotFoundException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.InetAddress;
 import java.net.URI;
@@ -10,30 +11,29 @@ public class MagicClientDriver {
 
 	public static void main(String[] args) {
 		
-		MagicClient magicClient = null;
-		
-		String stringHost = "";
-		String protocol = ""; 
-		int assumedPort = 0; //Check if valid in TCP Client
+		InetAddress clientHost = null;
 		String flag = ""; // Set to empty string so I can check if the user inputs a flag
-		int port = 6789;
+		int port = 9876;
 		
 		
-		if(args.length == 0 || args.length == 1){
+		if(args.length == 0 || args.length == 1){ // If the use enters in the wrong # of arguments
 			System.out.println("Usage: \t<Protocol> <IP Address or Hostname> [<Port Number or Flag>] [<Flag>]");
+			System.exit(1);
 		} else {
-			args[0] = args[0].toUpperCase();
-			if(!args[0].equals("TWP") && !args[0].equals("UDP")){
+			args[0] = args[0].toUpperCase(); // Makes the protocol enter go to Upper case
+			if(!args[0].equals("TCP") && !args[0].equals("UDP")){
 				System.out.println("Please enter in either TCP or UDP for <Protocol>\nUsage: \t<Protocol> <IP Address or Hostname> [<Port Number>] [<Flag>]");
+				System.exit(1);
 			} else {
-				protocol = args[0]; // Has to equal UDP or TCP
-				//Socket(String host, int port)
-				stringHost = args[1]; // Assume they don't enter an IP Address
-//				InetAddress hostIPAddress = InetAddress.getByName(args[1]);  
-//				String hostAddress = hostIPAddress.getHostAddress();
-//				System.out.println(hostAddress);
+				try {
+					InetAddress hostAddress = InetAddress.getByName(args[1]);
+					clientHost = hostAddress;
+				} catch (UnknownHostException e) {
+					System.out.println("Unknown Host Exception");
+					System.exit(1);
+				} 
 				if (args.length == 3){ // If user enters in 3 CMA
-					if(args[2].startsWith("-")){
+					if(args[2].startsWith("-")){ // If the 3rd CMA is a flag i.s -A -L
 						args[2].toUpperCase();
 						if(args[2].equals("-A") || args[2].equals("-L") || args[2].equals("-C")
 							|| args[2].equals("-S") || args[2].equals("-LC") || args[2].equals("-CL") 
@@ -44,15 +44,13 @@ public class MagicClientDriver {
 									+ "Usage: \t<Protocol> <IP Address or Hostname> [<Port Number or Flag>] [<Flag>]");
 							System.exit(1);
 						}
-					} else { // If its not a flag then its a port.
-						assumedPort = Integer.parseInt(args[2]);
-						port = assumedPort; // If port isn't valid (sdjf) Client will handle in Socket.
+				} else { // If its not a flag then its a port.
+						port = Integer.parseInt(args[2]);
 					}
 				}
 					
 				if(args.length == 4){ // If the user enters in 4 CMA
-					assumedPort = Integer.parseInt(args[2]);
-					port = assumedPort; // If port isn't valid (sdjf) Client will handle in Socket.
+					port = Integer.parseInt(args[2]); // Port is assumed to be the 3rd CMA
 					if(args[3].equals("-A") || args[3].equals("-L") || args[3].equals("-C")
 							|| args[3].equals("-S") || args[3].equals("-LC") || args[3].equals("-CL") 
 									|| args[3].equals("-SL") || args[3].equals("-SC") || args[3].equals("-CS")){
@@ -65,19 +63,11 @@ public class MagicClientDriver {
 				}
 			}
 		}
-			// Sending Host, Port and Flag in that Order
-			String stream = "" + stringHost + " " + port + " " +flag;
-			PrintStream fancyStream = null;
-			try {
-				fancyStream = new PrintStream(stream);
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		
+		if (args[0].equals("TCP")){
 			
 			
-			
-			//magicClient.printToStream(fancyStream);
+		}
 				
 	}
 }
