6064f8e34547ee97ac395000c147e628c2c5d931
Vanessa Perry <vanessap1590@gmail.com>
2017-11-08 02:50:10 -0500

    VP 11/8

--- src/server/TcpMagicServer.java
+++ src/server/TcpMagicServer.java
@@ -2,15 +2,20 @@ package server;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.SocketTimeoutException;
+import java.io.InterruptedIOException;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class TcpMagicServer {
+import common.Card;
+
+public class TcpMagicServer extends AbstractMagicServer{
 
 	/** Listen for clients knocking at the door (port) */
 	ServerSocket welcomeSocket;
+	
 
 	/**
 	 * Create a Server with a listening socket to accept client connections
@@ -19,87 +24,57 @@ public class TcpMagicServer {
 	 *
 	 * @throws IOException if we cannot create a socket.
 	 */
-	public TcpMagicServer(int port) throws IOException {
+	public TcpMagicServer(int port) {
+		super(port);
 		// socket listens for incoming connections.
-		welcomeSocket = new ServerSocket(port);
+		try {
+			welcomeSocket = new ServerSocket(getPort());	
+			listen();
+		} catch (IllegalArgumentException e) {
+			System.out.println("IllegalArgumentException, method was passed an illegal or ina"
+					+ "ppropriate argument, Try Again");
+			System.exit(1);
+		} catch (IOException e){
+			
+		}
+	
 	}
 
 	/**
-	 * Accept connections from clients, Get data they send and send it back
-	 * modified. Makes MANY assumptions. -- <b>For example puposes only!</b>
-	 *
-	 * @throws IOException if we have problems with our sockets.
+	 * Listens to requests from clients
+	 * @throws IOException 
 	 */
-	public void go() throws IOException {
+	@SuppressWarnings("resource")
+	public void listen() throws IOException {
 
-		// THIS IS NOT A VALID WAY TO WAIT FOR SOCKET CONNECTIONS!, You should
-		// not have a forever loop or while(true) 
-
-		welcomeSocket.setSoTimeout(10000);
-		//DataOutputStream clientOut;
-		//Scanner clientIn;    	
-		try{
+		welcomeSocket.setSoTimeout(60000); // Timeout in 1 min
+		try {
 			while(welcomeSocket.getSoTimeout()>0){
-				//for (; ;) {
-				System.out.println("Waiting for client on port " +
-						welcomeSocket.getLocalPort() + "...");
-				// Accept a connection, and create a new 'direct' socket
-				// This socket has the same port as the welcome socket.
+//				System.out.println("Waiting for client on port " +
+//					welcomeSocket.getLocalPort() + "...");
+			// Accept a connection, and create a new 'direct' socket
+			// This socket has the same port as the welcome socket.
 				Socket connectionSocket = welcomeSocket.accept();
 
-				// create a Scanner (stream) connected to the client's socket
+				ObjectOutputStream outToClient = new ObjectOutputStream(connectionSocket.getOutputStream());
 				Scanner clientIn = new Scanner(connectionSocket.getInputStream());
-				DataOutputStream clientOut = new DataOutputStream(connectionSocket.getOutputStream());
-
-				// read from the socket
 				String clientLine = clientIn.nextLine();
+				//System.out.println(clientLine);
 
-				// modify the data and send it back though the socket.
-				// don't forget the newline, the client expects one!
-				String modLine = clientLine.toUpperCase();
-				String message = "";
+				CardSource test = new CardSource();
+				ArrayList<Card> deck = null;
+			
+				setCardsReturned(clientLine);
+				deck = test.makeDeck(getItemToSend(), getTypes());            
+				
+				outToClient.writeObject(deck);
 
-				if(modLine.equals("-A")){
-					message = "Will return 60 cards of all types";
-				}
-				else if(modLine.equals("-L")){
-					message = "Will return 20 cards of Land type";
-				}
-				else if(modLine.equals("-C")){
-					message = "Will return 20 cards of Creature type";
-				}
-				else if(modLine.equals("-S")){
-					message = "Will return 20 cards of Spell type";
-				}
-				else{
-					message = "Error, not an acceptable param";
-					break;
-				}
-
-				clientOut.writeBytes(message + "\n");
-				//clientIn.close();
+				DataOutputStream clientOut = new DataOutputStream(connectionSocket.getOutputStream());
+				clientOut.writeBytes("\n");
 			}
-		}
-		catch(SocketTimeoutException stoe){
-			System.out.println("The server has timed out, please try again later");
-		}
-		//System.out.println("Exited loop" + "\n");
-		//clientIn.close();
-	}
 
-	/**
-	 * Main is a method that should be fully commented!
-	 *
-	 * @param args not used.
-	 */
-	public static void main(String[] args){
-
-		try {
-			TcpMagicServer server = new TcpMagicServer(6789);
-			server.go();
-		}
-		catch(IOException ioe) {
-			ioe.printStackTrace();
+		} catch (InterruptedIOException e){
+			System.out.println("Server Terminated due to Inactivity");	
 		}
 
 	}
