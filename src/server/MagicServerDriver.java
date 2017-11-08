0a2803d43975e5e35feafd0424617164fc47889c
Vanessa Perry <vanessap1590@gmail.com>
2017-11-08 02:48:12 -0500

    VP 11/8

--- src/server/MagicServerDriver.java
+++ src/server/MagicServerDriver.java
@@ -3,6 +3,32 @@ package server;
 public class MagicServerDriver {
 
 	public static void main(String[] args){
-		System.out.println("Main Method");
+		
+		int defaultPort = 6789;
+		
+		if (args.length > 2 || args.length == 0){
+			System.out.println("Usage: \t<Protocol> [<Port Number>]");
+			System.exit(1);
+		}
+		if (args.length == 1){
+			args[0] = args[0].toUpperCase(); // Makes the protocol enter go to Upper case
+			if(!args[0].equals("TCP") && !args[0].equals("UDP")){
+				System.out.println("Please enter in either TCP or UDP for <Protocol>\nUsage: \t<Protocol> <IP Address or Hostname> [<Port Number>] [<Flag>]");
+				System.exit(1);
+			} 
+		}
+		if (args.length == 2){
+			args[0] = args[0].toUpperCase(); // Makes the protocol enter go to Upper case
+			if(!args[0].equals("TCP") && !args[0].equals("UDP")){
+				System.out.println("Please enter in either TCP or UDP for <Protocol>\nUsage: \t<Protocol> <IP Address or Hostname> [<Port Number>] [<Flag>]");
+				System.exit(1);
+			} else {
+				defaultPort = Integer.parseInt(args[1]);
+			}
+		}
+		
+		if (args[0].equals("TCP")){
+			MagicServer ms = new TcpMagicServer(defaultPort);
+		}
 	}
 }
