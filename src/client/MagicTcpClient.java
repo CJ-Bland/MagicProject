975f6cb50783db369ff3a14f818db189fb8cbe30
Vanessa Perry <vanessap1590@gmail.com>
2017-11-08 02:43:06 -0500

    VP 11/8

--- src/client/MagicTcpClient.java
+++ src/client/MagicTcpClient.java
@@ -6,8 +6,6 @@ import java.io.ObjectInputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 import common.Card;
 
 
@@ -25,25 +23,10 @@ public class MagicTcpClient extends AbstractMagicClient{
 	 * @throws ClassNotFoundException 
 	 */
 	public MagicTcpClient(InetAddress clientHost, int clientPort, String clientFlag) throws IOException, ClassNotFoundException {
+		super(clientHost, clientPort, clientFlag);
 		
-		this.host = clientHost;
-		this.flag = clientFlag;
-		this.port = clientPort;
-	
-		clientSocket = new Socket(clientHost, port);
+		clientSocket = new Socket(clientHost, getPort());
 		go();
-	}
-
-	protected InetAddress getHost(){
-		return host;
-	}
-	
-	protected int getPort(){
-		return port;
-	}
-	
-	protected String getFlag(){
-		return flag;
 	}
 	
 	/**
@@ -53,47 +36,25 @@ public class MagicTcpClient extends AbstractMagicClient{
 	 * @throws IOException if something goes wrong with out socket.
 	 * @throws ClassNotFoundException 
 	 */
+	@SuppressWarnings("unchecked")
 	public void go() throws IOException, ClassNotFoundException {
 		
 		 ObjectInputStream inFromServer = new ObjectInputStream(clientSocket.getInputStream());
 		 DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
-		 toServer.writeBytes(flag + "\n");       // write to the server
+		 toServer.writeBytes(getFlag() + "\n");       // write to the server
 			
 
 
 		 ArrayList<Card> testCard = null;
 		 testCard = (ArrayList<Card>) inFromServer.readObject();
 		 
-		 //System.out.println(testCard.toString());
 		 for(int i = 0; i<testCard.size(); i++){
 			 System.out.println(testCard.get(i));
 		 }
 		 
 		 inFromServer.close();
 		 clientSocket.close();
-		 
-		/*
-		// Create a 'stream' connected to the keyboard for user input.
-		Scanner scanIn = new Scanner(System.in);
-		// Create a 'stream' connected to the server to write data.
-		DataOutputStream toServer = new DataOutputStream(clientSocket.getOutputStream());
-		// Create a 'stream' connected to the server to read data.
-		Scanner clientIn = new Scanner(clientSocket.getInputStream());
-		
-		//while(!sendLine.equals("end")){
-			System.out.print("Enter a sentence to send to the server >");
-			String sendLine = scanIn.nextLine();        // data from user
-			// Why am I sending a newline?
-			toServer.writeBytes(sendLine + "\n");       // write to the server
-			String recdLine = clientIn.nextLine();      // read from server
-			System.out.println("The server sent back:");
-			System.out.println("\t" + recdLine);
-		//}
-		clientSocket.close();                       // close the TCP connection.
-		clientIn.close();
-		*/
+	
 	}
-
-
 
 }
